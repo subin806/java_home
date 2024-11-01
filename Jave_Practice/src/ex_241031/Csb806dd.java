945a1ed94864fcb61fabd48a7edd73ff802dae81
subin806 <chosb0806@naver.com>
2024-11-01 09:23:47 +0900

    tnqls

---
+++ Jave_Practice/src/ex_241031/Csb806dd.java
@@ -0,0 +1,14 @@
+package ex_241031;
+
+public class Csb806dd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+private void sysout() {
+	// TODO Auto-generated method stub
+
+}
+}
+
